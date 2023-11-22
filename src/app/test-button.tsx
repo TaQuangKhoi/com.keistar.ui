7e690d62fa830a6dd6249ac1c648734ade563320
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-22 22:33:00 +0700

    Test Import TestButton

---
+++ src/app/test-button.tsx
@@ -0,0 +1,8 @@
+export default function TestButton() {
+    return (
+        <button
+            className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    )
+}
