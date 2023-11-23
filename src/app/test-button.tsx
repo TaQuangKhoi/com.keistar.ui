5d01158d52beee1dd371d53f4e63131a97a18801
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-23 19:49:44 +0700

    Done authentication page

--- src/app/test-button.tsx
+++ src/app/test-button.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function TestButton() {
     return (
-        <button
-            className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded">
+        <Link href="/authentication"
+            className="m-2 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded">
             Button
-        </button>
+        </Link>
     )
 }
