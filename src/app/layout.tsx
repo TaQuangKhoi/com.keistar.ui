8796c06611abb303571008d6e937302864d5e0d0
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-23 20:17:06 +0700

    Enable Dark Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google'
 import './globals.css'
 import {twMerge} from "tailwind-merge";
 import {Analytics} from "@/components/analytics";
+import {ThemeProvider} from "@/components/theme-provider";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -19,7 +20,14 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
         <Analytics/>
         </body>
         </html>
