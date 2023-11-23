485d9e0ee0f51590ba72b938d523941ec1f7b511
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-24 00:35:53 +0700

    Switch between Sign In and Create Account

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,29 +1,37 @@
+'use client'
 import {Metadata} from "next"
-import Link from "next/link"
 
 import {cn} from "@/lib/utils"
-import {buttonVariants} from "@/components/ui/button"
-import {UserAuthForm} from "@/app/authentication/components/user-auth-form"
+import {Button, buttonVariants} from "@/components/ui/button"
+import {CreateAccount} from "@/app/authentication/components/create-account";
+import {useState} from "react";
+import {SignIn} from "@/app/authentication/components/sign-in";
 
-export const metadata: Metadata = {
-    title: "Authentication",
-    description: "Authentication forms built using the components.",
-}
+// export const metadata: Metadata = {
+//     title: "Authentication",
+//     description: "Authentication forms built using the components.",
+// }
 
 export default function AuthenticationPage() {
+    const [isLoginMode, setIsLoginMode] = useState(true)
+
+    console.log("AuthenticationPage Render")
+
+    function handleLogin() {
+        setIsLoginMode(!isLoginMode)
+    }
+
     return (
         <>
             <div
                 className="min-h-screen flex-col items-center justify-center md:grid lg:grid-cols-2 lg:p-0">
-                <Link
-                    href="/authentication"
-                    className={cn(
-                        buttonVariants({variant: "ghost"}),
-                        "absolute right-4 top-4 md:right-8 md:top-8"
-                    )}
-                >
-                    Login
-                </Link>
+                <Button variant={"ghost"}
+                        onClick={handleLogin}
+                        className={cn("absolute right-4 top-4 md:right-8 md:top-8")}>
+                    {
+                        isLoginMode ? "Create an account" : "Sign in"
+                    }
+                </Button>
                 <div className="relative hidden lg:h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
                     <div className="absolute inset-0 bg-zinc-900"/>
                     <div className="relative z-20 flex items-center text-lg font-medium">
@@ -51,36 +59,9 @@ export default function AuthenticationPage() {
                         </blockquote>
                     </div>
                 </div>
-                <div className="lg:p-8">
-                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                        <div className="flex flex-col space-y-2 text-center">
-                            <h1 className="text-2xl font-semibold tracking-tight">
-                                Create an account
-                            </h1>
-                            <p className="text-sm text-muted-foreground">
-                                Enter your email below to create your account
-                            </p>
-                        </div>
-                        <UserAuthForm/>
-                        <p className="px-8 text-center text-sm text-muted-foreground">
-                            By clicking continue, you agree to our{" "}
-                            <Link
-                                href="/terms"
-                                className="underline underline-offset-4 hover:text-primary"
-                            >
-                                Terms of Service
-                            </Link>{" "}
-                            and{" "}
-                            <Link
-                                href="/privacy"
-                                className="underline underline-offset-4 hover:text-primary"
-                            >
-                                Privacy Policy
-                            </Link>
-                            .
-                        </p>
-                    </div>
-                </div>
+                {
+                    isLoginMode ? <SignIn/> : <CreateAccount/>
+                }
             </div>
         </>
     )
