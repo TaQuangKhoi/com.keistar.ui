76bda80767621fa6a5b4868912833fd9be424171
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-28 22:57:28 +0700

    Add System option

--- src/app/workspace/settings/appearance/appearance-form.tsx
+++ src/app/workspace/settings/appearance/appearance-form.tsx
@@ -21,7 +21,7 @@ import { toast } from "@/components/ui/use-toast"
 import { useTheme } from "next-themes"
 
 const appearanceFormSchema = z.object({
-  theme: z.enum(["light", "dark"], {
+  theme: z.enum(["light", "dark", "system"], {
     required_error: "Please select a theme.",
   }),
   font: z.enum(["inter", "manrope", "system"], {
@@ -157,6 +157,32 @@ export function AppearanceForm() {
                     </span>
                   </FormLabel>
                 </FormItem>
+                  <FormItem>
+                      <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
+                          <FormControl>
+                              <RadioGroupItem value="system" className="sr-only" />
+                          </FormControl>
+                            <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
+                                <div className="space-y-2 rounded-sm bg-slate-950 p-2">
+                                    <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                                        <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
+                                        <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                                    </div>
+                                    <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                                        <div className="h-4 w-4 rounded-full bg-slate-400" />
+                                        <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                                    </div>
+                                    <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                                        <div className="h-4 w-4 rounded-full bg-slate-400" />
+                                        <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                                    </div>
+                                </div>
+                            </div>
+                          <span className="block w-full p-2 text-center font-normal">
+                      System
+                    </span>
+                      </FormLabel>
+                  </FormItem>
               </RadioGroup>
             </FormItem>
           )}
