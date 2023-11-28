1336f492661cbd2d20b76e1042f320ecf04c77d1
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-29 00:20:08 +0700

    Add Page New E-leave

---
+++ src/app/workspace/office/e-leave/new-e-leave/page.tsx
@@ -0,0 +1,7 @@
+export default function New_E_leavePage() {
+    return (
+        <div>
+            <h1>New E-Leave</h1>
+        </div>
+    )
+}
--- src/app/workspace/office/e-leave/page.tsx
+++ src/app/workspace/office/e-leave/page.tsx
@@ -1,7 +1,14 @@
+import {buttonVariants} from "@/components/ui/button";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+
 export default function E_leavePage() {
     return (
         <div>
             <h1>E-Leave - Coming soon</h1>
+            <Link href={'e-leave/new-e-leave'} className={cn(buttonVariants({variant: "default"}))}>
+                New E-leave
+            </Link>
         </div>
     )
 }
