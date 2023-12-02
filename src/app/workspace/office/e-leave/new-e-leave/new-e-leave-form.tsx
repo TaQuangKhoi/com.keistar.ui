51cc4ae040af75197bb1a029d538d38acb475d21
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-02 12:16:11 +0700

    Use Grid to place Summary Card

--- src/app/workspace/office/e-leave/new-e-leave/new-e-leave-form.tsx
+++ src/app/workspace/office/e-leave/new-e-leave/new-e-leave-form.tsx
@@ -20,6 +20,8 @@ import {Button} from "@/components/ui/button";
 import {Label} from "@/components/ui/label";
 import DatePickerWithRange from "@/app/cards/components/date-picker-with-range";
 import {Textarea} from "@/components/ui/textarea";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {RecentSales} from "@/app/workspace/components/recent-sales";
 
 const newE_leaveFormSchema = z.object({
     leaveType: z
@@ -70,92 +72,104 @@ export function NewE_leaveForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <div className="flex-row space-y-2">
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <form onSubmit={form.handleSubmit(onSubmit)} className="col-span-2 space-y-8">
+                    <div className="flex-row space-y-2">
+                        <FormField
+                            control={form.control}
+                            name="leaveType"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Leave Type</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <FormControl>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select a leave type"/>
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            {
+                                                leaveTypes.map((leaveType, index) => (
+                                                    <SelectItem key={index} value={leaveType.value}>
+                                                        {leaveType.value}
+                                                    </SelectItem>
+                                                ))
+                                            }
+                                        </SelectContent>
+                                    </Select>
+                                    {/*<FormDescription>*/}
+                                    {/*    You can manage verified email addresses in your{" "}*/}
+                                    {/*    <Link href="/examples/forms">email settings</Link>.*/}
+                                    {/*</FormDescription>*/}
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="rememberMe"
+                            render={({field}) => (
+                                <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                                    <FormControl>
+                                        <Checkbox
+                                            checked={field.value}
+                                            onCheckedChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                    <div className="space-y-1 leading-none">
+                                        <FormLabel>
+                                            Remember me
+                                        </FormLabel>
+                                        {/*<FormDescription>*/}
+                                        {/*    You can manage your mobile notifications in the{" "}*/}
+                                        {/*    <Link href="/examples/forms">mobile settings</Link> page.*/}
+                                        {/*</FormDescription>*/}
+                                    </div>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="date" className="shrink-0">
+                            Pick a date
+                        </Label>
+                        <DatePickerWithRange className="[&>button]:w-[260px]"/>
+                    </div>
                     <FormField
                         control={form.control}
-                        name="leaveType"
+                        name="about"
                         render={({field}) => (
                             <FormItem>
-                                <FormLabel>Leave Type</FormLabel>
-                                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                    <FormControl>
-                                        <SelectTrigger>
-                                            <SelectValue placeholder="Select a leave type"/>
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        {
-                                            leaveTypes.map((leaveType, index) => (
-                                                <SelectItem key={index} value={leaveType.value}>
-                                                    {leaveType.value}
-                                                </SelectItem>
-                                            ))
-                                        }
-                                    </SelectContent>
-                                </Select>
+                                <FormLabel>About</FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                        placeholder="Include comments for your approver"
+                                        className="resize-none"
+                                        {...field}
+                                    />
+                                </FormControl>
                                 {/*<FormDescription>*/}
-                                {/*    You can manage verified email addresses in your{" "}*/}
-                                {/*    <Link href="/examples/forms">email settings</Link>.*/}
+                                {/*    You can <span>@mention</span> other users and organizations to*/}
+                                {/*    link to them.*/}
                                 {/*</FormDescription>*/}
                                 <FormMessage/>
                             </FormItem>
                         )}
                     />
-                    <FormField
-                        control={form.control}
-                        name="rememberMe"
-                        render={({field}) => (
-                            <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                                <FormControl>
-                                    <Checkbox
-                                        checked={field.value}
-                                        onCheckedChange={field.onChange}
-                                    />
-                                </FormControl>
-                                <div className="space-y-1 leading-none">
-                                    <FormLabel>
-                                        Remember me
-                                    </FormLabel>
-                                    {/*<FormDescription>*/}
-                                    {/*    You can manage your mobile notifications in the{" "}*/}
-                                    {/*    <Link href="/examples/forms">mobile settings</Link> page.*/}
-                                    {/*</FormDescription>*/}
-                                </div>
-                            </FormItem>
-                        )}
-                    />
-                </div>
-                <div className="space-y-2">
-                    <Label htmlFor="date" className="shrink-0">
-                        Pick a date
-                    </Label>
-                    <DatePickerWithRange className="[&>button]:w-[260px]"/>
-                </div>
-                <FormField
-                    control={form.control}
-                    name="about"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>About</FormLabel>
-                            <FormControl>
-                                <Textarea
-                                    placeholder="Include comments for your approver"
-                                    className="resize-none"
-                                    {...field}
-                                />
-                            </FormControl>
-                            {/*<FormDescription>*/}
-                            {/*    You can <span>@mention</span> other users and organizations to*/}
-                            {/*    link to them.*/}
-                            {/*</FormDescription>*/}
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Request Now</Button>
-            </form>
-
+                    <Button type="submit">Request Now</Button>
+                </form>
+                <Card className="col-span-2">
+                    <CardHeader>
+                        <CardTitle>Your Request Includes</CardTitle>
+                        <CardDescription>
+                            You made 265 sales this month.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <RecentSales/>
+                    </CardContent>
+                </Card>
+            </div>
         </Form>
     )
 }
