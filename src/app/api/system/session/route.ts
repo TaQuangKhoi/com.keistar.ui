9316103155ebb37e0b66eae7740528e3fa356570
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-05 13:43:16 +0700

    handle 401 in session API

--- src/app/api/system/session/route.ts
+++ src/app/api/system/session/route.ts
@@ -1,16 +1,56 @@
-import { cookies } from 'next/headers'
+import {cookies} from 'next/headers'
 
 export async function GET(request: Request) {
     const cookieStore = cookies()
-    const cookie = cookieStore.get('X-Bonita-API-Token')
-    console.log(cookie)
-    const res = await fetch('http://localhost:8080/bonita/API/system/session/1', {
+    const bonitaApiKey = cookieStore.get('X-Bonita-API-Token')
+    const jSessionId = cookieStore.get('JSESSIONID')
+
+    const error401Response = new Response(JSON.stringify({error: "No Bonita API Key and JSESSIONID cookie found"}), {
+        status: 401,
+        statusText: 'Unauthorized',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
         },
+    })
+
+    if (!bonitaApiKey && !jSessionId) {
+        return error401Response
+    }
+    if (!bonitaApiKey) {
+        console.log("jSessionId", jSessionId)
+        return error401Response
+    }
+    if (!jSessionId) {
+        return error401Response
+    }
+
+    const headers: HeadersInit = {
+        'X-Bonita-Api-Token': bonitaApiKey.value,
+        "JSESSIONID": jSessionId.value,
+    }
+    console.log("headers", headers)
+
+    const res = await fetch("http://localhost:28071/bonita/API/system/session/unusedId", {
+        headers: headers,
         method: 'GET',
     })
-    const data = await res.json()
 
-    return Response.json({ data })
+    if (res.status === 200) {
+        const data = await res.json()
+        console.log(data)
+        return Response.json({data})
+    } else {
+        console.log("res", res)
+        return new Response(
+            JSON.stringify({
+                error: "Failed to fetch session",
+            }
+        ), {
+            status: res.status,
+            statusText: res.statusText,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    }
 }
