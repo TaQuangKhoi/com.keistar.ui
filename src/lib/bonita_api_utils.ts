cf6d64756edbccb67f6ed0d31ca70ee35971008f
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-06 22:50:35 +0700

    Add func findUserById

--- src/lib/bonita_api_utils.ts
+++ src/lib/bonita_api_utils.ts
@@ -22,3 +22,28 @@ export async function getSession() {
     )
     return [result, status];
 }
+
+export async function findUserById(id) {
+    let url = 'http://localhost:28071/bonita/API/identity/user/' + id
+    let result = null
+    let status = null
+
+    await fetch(url,
+        {
+            credentials: "include",
+            mode: 'cors',
+        }
+    ).then(response => {
+        status = response.status
+        return response.json();
+    }).then(
+        data => {
+            result = data
+        }
+    ).catch(
+        error => {
+            console.log(error)
+        }
+    )
+    return [result, status];
+}
