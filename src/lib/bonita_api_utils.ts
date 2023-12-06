c2e09be1061a748cd6a6314a63410cba187168ee
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-06 23:44:04 +0700

    Update fetcher for bonita api

--- src/lib/bonita_api_utils.ts
+++ src/lib/bonita_api_utils.ts
@@ -1,29 +1,23 @@
-export async function getSession() {
-    let url = 'http://localhost:28071/bonita/API/system/session/unusedId'
-    let result = null
-    let status = null
+import useSWR from 'swr'
 
-    await fetch(url,
-        {
-            credentials: "include",
-            mode: 'cors',
-        }
-    ).then(response => {
-        status = response.status
-        return response.json();
-    }).then(
-        data => {
-            result = data
-        }
-    ).catch(
-        error => {
-            console.log(error)
-        }
-    )
-    return [result, status];
+const fetcher = (url: string) => fetch(url, {
+        credentials: "include",
+        mode: 'cors',
+    }
+).then(res => res.json())
+
+export function useBonitaSession() {
+    let url = 'http://localhost:28071/bonita/API/system/session/unusedId'
+    const {data, error, isLoading} = useSWR(url, (url) => fetcher(url))
+
+    return {
+        session: data,
+        isSessionLoading: isLoading,
+        isError: error
+    }
 }
 
-export async function findUserById(id) {
+export async function findUserById(id: string) {
     let url = 'http://localhost:28071/bonita/API/identity/user/' + id
     let result = null
     let status = null
