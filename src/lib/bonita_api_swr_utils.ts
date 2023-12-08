bf216b3ec3539a2d3901c08d8f7631855a202a70
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-08 23:38:10 +0700

    usr global sessionApiUrl in bonita swt

--- src/lib/bonita_api_swr_utils.ts
+++ src/lib/bonita_api_swr_utils.ts
@@ -1,4 +1,5 @@
 import useSWR from 'swr'
+import {sessionApiUrl} from "@/lib/api-enpoints";
 
 const init: RequestInit = {
     credentials: "include",
@@ -7,9 +8,6 @@ const init: RequestInit = {
 
 const fetcher = (url: string) => fetch(url, init
 ).then(res => res.json())
-
-
-const sessionApiUrl = 'http://localhost:28071/bonita/API/system/session/unusedId'
 
 
 function useBonitaSession() {
