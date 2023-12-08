3370a48619206c053c954132b8a383d61e0088f5
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-08 23:39:10 +0700

    add process api handle for bonita with axios

---
+++ src/bonita/api/bpm/process.ts
@@ -0,0 +1,27 @@
+import {default as axios} from "@/lib/axios-instance";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies(null, {path: '/bonita'});
+const bonitaToken = cookies.get("X-Bonita-API-Token");
+
+function getProcess(processId: string) {
+    return "http://localhost:28071/bonita/API/bpm/process/" + processId;
+}
+
+interface ProcessInstantiationResponse {
+    caseId: string;
+}
+
+async function instantiateProcess(processId: string, contractInputs: any)  {
+    const url = getProcess(processId) + "/instantiation";
+    return await axios.post(url, contractInputs, {
+            withCredentials: true,
+        }
+    ).then((response) => {
+        return response.data;
+    })
+}
+
+export {
+    instantiateProcess,
+}
