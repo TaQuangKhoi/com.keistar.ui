7fd76c03b73cda81a254f8331afe5d86ac5e778b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-09 08:53:07 +0700

    Dont use localStorage

--- src/bonita/lib/utils.ts
+++ src/bonita/lib/utils.ts
@@ -1,20 +1,17 @@
-'use client';
-
 import {getCurrentUserSession} from "@/bonita/api/system/session";
-let localStorage = window.localStorage;
 
 function getBonitaAuthToken() {
-    return localStorage.getItem('x-bonita-api-token');
+    // return localStorage.getItem('x-bonita-api-token');
 }
 
 function setBonitaAuthToken(token: string) {
-    localStorage.setItem('x-bonita-api-token', token);
+    // localStorage.setItem('x-bonita-api-token', token);
 }
 
 async function saveBonitaAuthToken() {
     const res = await getCurrentUserSession();
     const token = res.headers['x-bonita-api-token'];
-    setBonitaAuthToken(token);
+    // setBonitaAuthToken(token);
 }
 
 export {
