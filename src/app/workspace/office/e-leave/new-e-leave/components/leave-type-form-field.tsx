0eb7ebc5ecd20e3217329080bc8fd9e9266ec559
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-11 00:17:52 +0700

    Update leave-type-form-field to get data from bdm

--- src/app/workspace/office/e-leave/new-e-leave/components/leave-type-form-field.tsx
+++ src/app/workspace/office/e-leave/new-e-leave/components/leave-type-form-field.tsx
@@ -1,6 +1,8 @@
 import {FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {UseFormReturn} from "react-hook-form";
+import {useEffect, useState} from "react";
+import {findsBusinessData} from "@/bonita/api/bdm/business-data-query";
 
 
 const leaveTypes = [
@@ -17,6 +19,17 @@ const leaveTypes = [
     {value: "Others"},
 ]
 
+interface LeaveType {
+    description: string,
+    isActive: boolean,
+    name: string,
+    persistenceId: number,
+    persistenceId_string: string,
+    persistenceVersion: number,
+    persistenceVersion_string: string,
+
+}
+
 
 export default function LeaveTypeFormField(
     {
@@ -27,6 +40,24 @@ export default function LeaveTypeFormField(
         name: string
     }
 ) {
+    const [leaveTypes, setLeaveTypes] = useState([] as LeaveType[])
+
+    useEffect(() => {
+        const getLeaveType = async () => {
+            await findsBusinessData(
+                "com.havako.model.office.LeaveType", "find", 0, 20
+            ).then(function (response) {
+
+                // set default value
+                form.setValue(name, response.data[0].persistenceId_string)
+
+                setLeaveTypes(response.data)
+            })
+        };
+
+        getLeaveType();
+    }, [])
+
     return (
         <FormField
             control={form.control}
@@ -34,22 +65,36 @@ export default function LeaveTypeFormField(
             render={({field}) => (
                 <FormItem>
                     <FormLabel>Leave Type</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                        <FormControl>
-                            <SelectTrigger>
-                                <SelectValue placeholder="Select a leave type"/>
-                            </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                            {
-                                leaveTypes.map((leaveType, index) => (
-                                    <SelectItem key={index} value={leaveType.value}>
-                                        {leaveType.value}
-                                    </SelectItem>
-                                ))
-                            }
-                        </SelectContent>
-                    </Select>
+                    {
+                        leaveTypes.length === 0 && (
+                            <Select>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Loading"/>
+                                </SelectTrigger>
+                            </Select>
+                        )
+                    }
+                    {
+                        leaveTypes.length > 0 && (
+                            <Select onValueChange={field.onChange} defaultValue={leaveTypes[0].persistenceId_string}>
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select a leave type"/>
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {
+                                        leaveTypes.map((leaveType, index) => (
+                                            <SelectItem key={index} value={leaveType.persistenceId_string}>
+                                                {leaveType.name}
+                                            </SelectItem>
+                                        ))
+                                    }
+                                </SelectContent>
+                            </Select>
+                        )
+                    }
+
                     {/*<FormDescription>*/}
                     {/*    You can manage verified email addresses in your{" "}*/}
                     {/*    <Link href="/examples/forms">email settings</Link>.*/}
