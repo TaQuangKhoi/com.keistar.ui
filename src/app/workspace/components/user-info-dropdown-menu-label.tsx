d4c232104365d81dac11a2a41696ddad8057f173
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-10 22:38:05 +0700

    get userName from atom with query

--- src/app/workspace/components/user-info-dropdown-menu-label.tsx
+++ src/app/workspace/components/user-info-dropdown-menu-label.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import {DropdownMenuLabel} from "@/components/ui/dropdown-menu";
+import {useAtomValue} from "jotai";
+import {userNameAtom} from "@/app/workspace/atoms";
 
 export default function UserInfoDropdownMenuLabel() {
+    const userName = useAtomValue(userNameAtom)
+
     return <DropdownMenuLabel className="font-normal">
         <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">
-                Hảo Văn
+                {userName}
             </p>
             <p className="text-xs leading-none text-muted-foreground">
-                m@example.com
+                haov348@gmail.com
             </p>
         </div>
     </DropdownMenuLabel>
