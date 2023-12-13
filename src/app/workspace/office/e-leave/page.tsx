ab8b4260d69ae0b8c351b07a496cb600b4664526
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-13 22:53:00 +0700

    Add Calendar to E-leave

--- src/app/workspace/office/e-leave/page.tsx
+++ src/app/workspace/office/e-leave/page.tsx
@@ -1,14 +1,15 @@
 import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
 import {cn} from "@/lib/utils";
+import {Calendar} from 'antd';
 
 export default function E_leavePage() {
     return (
         <div>
-            <h1>E-Leave - Coming soon</h1>
             <Link href={'e-leave/new-e-leave'} className={cn(buttonVariants({variant: "default"}))}>
                 New E-leave
             </Link>
+            <Calendar/>
         </div>
     )
 }
