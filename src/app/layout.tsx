1ead6d491a460ad14123e4832661f4e6a24b6505
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-30 15:10:41 +0700

    Use AntdRegistry
    follow this docs https://ant.design/docs/react/use-with-next#using-app-router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import {Analytics} from "@/components/analytics";
 import {ThemeProvider} from "@/components/theme-provider";
 import {Toaster} from "@/components/ui/toaster"
+import StyledComponentsRegistry from "@/lib/AntdRegistry";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -26,7 +27,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
         >
-            {children}
+            <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
             <Toaster/>
         </ThemeProvider>
         <Analytics/>
