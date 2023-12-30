80b69efc66d7275df064be01ae582e324052455b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-30 22:23:40 +0700

    Remove unuse hook

--- src/app/authentication/components/art-work.tsx
+++ src/app/authentication/components/art-work.tsx
@@ -1,12 +1,8 @@
 'use client'
 
 import Image from "next/image";
-import {useWindowSize} from "@uidotdev/usehooks";
 
 export default function ArtWork() {
-    const size = useWindowSize();
-    const scale = 2;
-
     return <div className="absolute inset-0 bg-zinc-900">
         <Image src='/haovan/authartwork.jpg' alt="Hảo Văn" fill={true} style={{objectFit: "cover"}}/>
     </div>
