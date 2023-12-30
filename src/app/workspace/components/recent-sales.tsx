5dc2e45a8f6e2b6bc81adbc1693632c2055dbf49
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-30 21:03:26 +0700

    Tách riêng Component SalesCard

--- src/app/workspace/components/recent-sales.tsx
+++ src/app/workspace/components/recent-sales.tsx
@@ -4,22 +4,26 @@ import {
     AvatarImage,
 } from "@/components/ui/avatar"
 
+function SalesCard() {
+    return <div className="flex items-center">
+        <Avatar className="h-9 w-9">
+            <AvatarImage src="/haovan/01.jpg" alt="Avatar"/>
+            <AvatarFallback>OM</AvatarFallback>
+        </Avatar>
+        <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">
+                olivia.martin@email.com
+            </p>
+        </div>
+        <div className="ml-auto font-medium">+$1,999.00</div>
+    </div>
+}
+
 export function RecentSales() {
     return (
         <div className="space-y-8">
-            <div className="flex items-center">
-                <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/01.png" alt="Avatar"/>
-                    <AvatarFallback>OM</AvatarFallback>
-                </Avatar>
-                <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">Olivia Martin</p>
-                    <p className="text-sm text-muted-foreground">
-                        olivia.martin@email.com
-                    </p>
-                </div>
-                <div className="ml-auto font-medium">+$1,999.00</div>
-            </div>
+            <SalesCard/>
             <div className="flex items-center">
                 <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
                     <AvatarImage src="/avatars/02.png" alt="Avatar"/>
