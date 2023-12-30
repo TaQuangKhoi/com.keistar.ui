6292e796d5c7a0383a89791bbd2fa3fff53c39eb
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-30 14:47:25 +0700

    Turn to function getProcessesByEleaveId

--- src/app/test/test-component.tsx
+++ src/app/test/test-component.tsx
@@ -15,24 +15,30 @@ interface ContextRef {
     "storageId_string": string, // "147"
 }
 
+
 export default function TestComponent() {
-    async function getProcesses() {
-        const data = await findsArchivedProcessInstances();
-        let ids: string[] = [];
-        data.forEach((item: ArchivedProcessInstance) => {
-            ids.push(item.id)
-        })
-        let e_leaves = [];
-        for (let i = 0; i < ids.length; i++) {
-            console.debug(ids[i])
-            const e_leave = await findsContextByArchivedProcessInstanceID(ids[i])
-            console.debug(e_leave)
-            e_leaves.push(e_leave)
+    async function getProcessesByEleaveId(eleaveId: string) {
+        console.debug("getProcessesByEleaveId", eleaveId)
+        const data: ArchivedProcessInstance[] = await findsArchivedProcessInstances();
+
+        let processInstances: ArchivedProcessInstance[] = [];
+
+        for (let i = 0; i < data.length; i++) {
+            const context: ContextRef[] = await findsContextByArchivedProcessInstanceID(data[i].id)
+
+            // List all eleave type object ref
+            for (const [key, value] of Object.entries(context)) {
+                if (value.type === "com.havako.model.office.Eleave" && value.storageId_string === eleaveId) {
+                    processInstances.push(data)
+                }
+            }
         }
+
+        console.debug(processInstances)
     }
 
     return (
-        <Button onClick={getProcesses}>
+        <Button onClick={() => getProcessesByEleaveId("143")}>
             Test Button
         </Button>
     )
