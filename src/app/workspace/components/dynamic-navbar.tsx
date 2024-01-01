0b8ead6c20819da5673d6b1cf041e3024e475e4e
Tạ Quang Khôi <khoi0941@gmail.com>
2024-01-01 20:31:43 +0700

    Done Dynamic NavBar

--- src/app/workspace/components/dynamic-navbar.tsx
+++ src/app/workspace/components/dynamic-navbar.tsx
@@ -1,17 +1,49 @@
+'use client';
+
 import TeamSwitcher from "@/app/workspace/components/team-switcher";
 import {MainNav} from "@/app/workspace/components/main-nav";
 import {Search} from "@/app/workspace/components/search";
 import Notification from "@/app/workspace/components/Notification";
 import {UserNav} from "@/app/workspace/components/user-nav";
 
-export default function DynamicNavbar() {
-    return <>
-        <TeamSwitcher/>
-        <MainNav className="mx-6"/>
-        <div className="ml-auto flex items-center space-x-4">
-            <Search/>
-            <Notification/>
-            <UserNav/>
+import {useWindowScroll} from "@uidotdev/usehooks";
+
+function FullNavBar() {
+    return (
+        <div className="border-b top-0 bg-white dark:bg-slate-900">
+            <div className="flex h-16 items-center px-4">
+                <TeamSwitcher/>
+                <MainNav className="mx-6"/>
+                <div className="ml-auto flex items-center space-x-4">
+                    <Search/>
+                    <Notification/>
+                    <UserNav/>
+                </div>
+            </div>
         </div>
+    )
+}
+
+function CompactNavBar() {
+    return (
+        <div className="sticky border-b top-0 bg-white dark:bg-slate-900">
+            <div className="flex h-16 items-center px-4">
+                <MainNav className="mx-6"/>
+            </div>
+        </div>
+    )
+}
+
+export default function DynamicNavbar() {
+    const [{x, y}, scrollTo] = useWindowScroll();
+
+    // @ts-ignore
+    return <>
+        {
+            y === 0 && <FullNavBar/>
+        }
+        {
+            y > 10 && <CompactNavBar/>
+        }
     </>
 }
--- src/app/workspace/layout.tsx
+++ src/app/workspace/layout.tsx
@@ -7,11 +7,7 @@ interface WorkspaceLayoutProps {
 export default function WorkspaceLayout({children}: WorkspaceLayoutProps) {
     return (
         <div className="flex-col md:flex">
-            <div className="border-b sticky top-0 bg-white dark:bg-slate-900">
-                <div className="flex h-16 items-center px-4">
-                    <DynamicNavbar/>
-                </div>
-            </div>
+            <DynamicNavbar/>
             <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
         </div>
     )
