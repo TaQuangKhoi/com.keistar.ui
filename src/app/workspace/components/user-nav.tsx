89ecac2f9f2b8ae51a68f0450aa783ddff242de2
Tạ Quang Khôi <khoi0941@gmail.com>
2024-01-01 07:34:04 +0700

    Change image of UserNav

--- src/app/workspace/components/user-nav.tsx
+++ src/app/workspace/components/user-nav.tsx
@@ -23,8 +23,8 @@ export function UserNav() {
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="/avatars/01.png" alt="@shadcn"/>
-                        <AvatarFallback>SC</AvatarFallback>
+                        <AvatarImage src="/haovan/01.jpg" alt="@haov348"/>
+                        <AvatarFallback>HV</AvatarFallback>
                     </Avatar>
                 </Button>
             </DropdownMenuTrigger>
