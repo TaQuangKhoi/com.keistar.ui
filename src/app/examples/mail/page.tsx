ea738f1365a44f7070a130cd54df5619cf41e538
Tạ Quang Khôi <khoi0941@gmail.com>
2024-01-10 23:37:59 +0700

    Remove redundant Image in Mail Page

--- src/app/examples/mail/page.tsx
+++ src/app/examples/mail/page.tsx
@@ -13,22 +13,6 @@ export default function MailPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </div>
       <div className="hidden flex-col md:flex">
         <Mail
           accounts={accounts}
