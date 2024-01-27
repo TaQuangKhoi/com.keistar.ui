8d7a2ce4d4217f7e0f07ac78a35a88413c20fa30
Tạ Quang Khôi <khoi0941@gmail.com>
2024-01-27 16:24:17 +0700

    Done fake data

--- src/app/workspace/tasks/components/task-list.tsx
+++ src/app/workspace/tasks/components/task-list.tsx
@@ -2,23 +2,51 @@ import {ScrollArea} from "@/components/ui/scroll-area"
 import {cn} from "@/lib/utils";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import {Badge} from "@/components/ui/badge";
-import {useMail} from "@/app/examples/mail/use-mail";
+import { faker } from '@faker-js/faker';
+import {useTask} from "@/app/workspace/tasks/use-task";
+import {HumanTask} from "@/bonita/api/bpm/human-task/types";
 
-const items = [
+const items: HumanTask[] = [
     {
-        id: "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
-        name: "Review Eleave",
-        email: "williamsmith@example.com",
-        subject: "Create Eleave Process",
-        text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next steps to ensure the project's success.\n\nPlease come prepared with any questions or insights you may have. Looking forward to our meeting!\n\nBest regards, William",
-        date: "2023-10-22T09:00:00",
-        read: true,
-        labels: ["meeting", "work", "important"],
+        displayDescription: "",
+        executedBy: "0",
+        rootContainerId: {
+            displayDescription: "",
+            deploymentDate: "2024-01-27 15:49:53.807",
+            displayName: "Create E-Leave Request",
+            name: "Create_Eleave",
+            description: "",
+            deployedBy: "103",
+            id: "8569231201909689792",
+            activationState: "ENABLED",
+            version: "1.3.0.alpha",
+            configurationState: "RESOLVED",
+            last_update_date: "2024-01-27 15:49:54.771",
+            actorinitiatorid: "508"
+        },
+        assigned_date: "2024-01-27 15:50:38.255",
+        displayName: "Review E-leave",
+        executedBySubstitute: "0",
+        dueDate: "",
+        description: "",
+        type: "USER_TASK",
+        priority: "normal",
+        actorId: "508",
+        processId: "8569231201909689792",
+        caseId: "7002",
+        name: "Review E-leave",
+        reached_state_date: "2024-01-27 15:50:38.258",
+        rootCaseId: "7002",
+        id: "140007",
+        state: "ready",
+        parentCaseId: "7002",
+        last_update_date: "2024-01-27 15:50:38.258",
+        assigned_id: "103"
     }
 ]
 
 export default function TaskList() {
-    const [mail, setMail] = useMail()
+    const [mail, setMail] = useTask()
 
     return (
         <ScrollArea className="h-screen">
@@ -40,10 +68,10 @@ export default function TaskList() {
                         <div className="flex w-full flex-col gap-1">
                             <div className="flex items-center">
                                 <div className="flex items-center gap-2">
-                                    <div className="font-semibold">{item.name}</div>
-                                    {!item.read && (
-                                        <span className="flex h-2 w-2 rounded-full bg-blue-600"/>
-                                    )}
+                                    <div className="font-semibold">{item.displayName}</div>
+                                    {/*{!item.read && (*/}
+                                    {/*    <span className="flex h-2 w-2 rounded-full bg-blue-600"/>*/}
+                                    {/*)}*/}
                                 </div>
                                 <div
                                     className={cn(
@@ -53,25 +81,29 @@ export default function TaskList() {
                                             : "text-muted-foreground"
                                     )}
                                 >
-                                    {formatDistanceToNow(new Date(item.date), {
+                                    {formatDistanceToNow(new Date(item.assigned_date), {
                                         addSuffix: true,
                                     })}
                                 </div>
                             </div>
-                            <div className="text-xs font-medium">{item.subject}</div>
+
+                            {/** Process name */}
+                            <div className="text-xs font-medium">
+                                Process: {item.rootContainerId.displayName}
+                            </div>
                         </div>
                         <div className="line-clamp-2 text-xs text-muted-foreground">
-                            {item.text.substring(0, 300)}
+                            {item.rootContainerId.displayDescription.substring(0, 300)}
                         </div>
-                        {item.labels.length ? (
-                            <div className="flex items-center gap-2">
-                                {item.labels.map((label) => (
-                                    <Badge key={label} variant="default">
-                                        {label}
-                                    </Badge>
-                                ))}
-                            </div>
-                        ) : null}
+                        {/*{item.labels.length ? (*/}
+                        {/*    <div className="flex items-center gap-2">*/}
+                        {/*        {item.labels.map((label) => (*/}
+                        {/*            <Badge key={label} variant="default">*/}
+                        {/*                {label}*/}
+                        {/*            </Badge>*/}
+                        {/*        ))}*/}
+                        {/*    </div>*/}
+                        {/*) : null}*/}
                     </button>
                 ))}
             </div>
