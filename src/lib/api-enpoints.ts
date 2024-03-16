a8d2b944149a22e5fbad62a7849eac53a4163a30
Tạ Quang Khôi <khoi0941@gmail.com>
2024-03-16 17:01:46 +0700

    Restore api-endpoints.ts

--- src/lib/api-enpoints.ts
+++ src/lib/api-enpoints.ts
@@ -5,7 +5,7 @@ import {getBaseUrl} from "@/lib/axios-instance";
 /**
  * NEXT_PUBLIC_BONITA_URL=http://localhost:{portNumber}/bonita
  */
-const sessionApiUrl = getBaseUrl('/API/system/session/unusedId', window.location.hostname)
+const sessionApiUrl = '/API/system/session/unusedId'
 
 export {
     sessionApiUrl
