fab64dec5c2ad886f2e5dd76c93d241301660a02
Tạ Quang Khôi <khoi0941@gmail.com>
2024-03-16 20:04:14 +0700

    Fix async function logout()

--- src/bonita/api/identity/authentication.ts
+++ src/bonita/api/identity/authentication.ts
@@ -1,7 +1,7 @@
 import {default as axios, getBaseUrl} from "@/lib/axios-instance";
 
 async function logout() {
-    return await axios.get(getBaseUrl('/logoutservice', window.location.hostname), {})
+    return await axios.get(<string>getBaseUrl('/logoutservice', window.location.hostname), {})
 }
 
 export {logout}
