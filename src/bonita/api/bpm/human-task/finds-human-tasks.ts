a7218fd4598014c1fb56ffff6f26f65b0c84dace
Tạ Quang Khôi <khoi0941@gmail.com>
2024-03-17 10:48:35 +0700

    Reduce code in findsHumanTasks

--- src/bonita/api/bpm/human-task/finds-human-tasks.ts
+++ src/bonita/api/bpm/human-task/finds-human-tasks.ts
@@ -1,8 +1,7 @@
 import {default as axios, getBaseUrl} from "@/lib/axios-instance";
+import buildUrlParameter from "@/bonita/lib/build-url-parameter";
 
-function isStartOfQueryString(url: string) {
-    return url.indexOf("?") === -1;
-}
+
 
 /**
  * Finds HumanTasks with pagination params and filters
@@ -21,48 +20,7 @@ async function findsHumanTasks(
 ) {
     let url = <string>getBaseUrl('/API/bpm/humanTask', window.location.hostname)
 
-    // Add p to the url if it is not null
-    url = url.concat(`?p=${p}`);
-
-    // Add c to the url if it is not null
-    url = url.concat(`&c=${c}`);
-
-    // Add f to the url if it is not null
-    if (f) {
-        if (isStartOfQueryString(url)) {
-            url = url.concat(`?f=${f}`);
-        } else {
-            url = url.concat(`&f=${f}`);
-        }
-    }
-
-    // Add o to the url if it is not null
-    if (o) {
-        if (isStartOfQueryString(url)) {
-            url = url.concat(`?o=${o}`);
-        } else {
-            url = url.concat(`&o=${o}`);
-        }
-    }
-
-    // Add s to the url if it is not null
-    if (s) {
-        if (isStartOfQueryString(url)) {
-            url = url.concat(`?s=${s}`);
-        } else {
-            url = url.concat(`&s=${s}`);
-        }
-    }
-
-    if (d) {
-        d.forEach((value) => {
-            if (isStartOfQueryString(url)) {
-                url = url.concat(`?d=${value}`);
-            } else {
-                url = url.concat(`&d=${value}`);
-            }
-        });
-    }
+    url = buildUrlParameter(url, p, c, f, o, s, d);
 
     return await axios.get(url, {
             withCredentials: true,
