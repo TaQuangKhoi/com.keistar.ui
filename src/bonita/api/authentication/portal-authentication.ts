f487b4061f388617e982536ca0e7be94a7e6f1d8
Tạ Quang Khôi <khoi0941@gmail.com>
2024-04-23 09:51:19 +0700

    Update login()

--- src/bonita/api/authentication/portal-authentication.ts
+++ src/bonita/api/authentication/portal-authentication.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {default as axios2, getBaseUrl} from "@/lib/axios-instance";
 
 async function login(username: string, password: string) {
-    return await axios.post(<string>getBaseUrl('/loginservice', window.location.hostname),
+    return await axios.post(<string>getBaseUrl('/loginservice', process.env.NEXT_PUBLIC_BONITA_HOSTNAME),
         `username=${username}&password=${password}&redirect=false&redirectURL=`,
         {
             withCredentials: true,
