b5bb2a94ced311b09749276c058015d406d98c50
Tạ Quang Khôi <khoi0941@gmail.com>
2024-04-24 09:03:09 +0700

    Update getBaseUrl

--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -28,7 +28,7 @@ function getBaseUrl(url: string | undefined, host: string | undefined) {
     if (process.env.NODE_ENV === 'development') {
         return `http://${host}:${process.env.NEXT_PUBLIC_BONITA_DEV_PORT}/bonita` + url;
     } else {
-        return url;
+        return `http://${host}:${process.env.NEXT_PUBLIC_BONITA_DEV_PORT}/bonita` + url;
     }
 }
 
