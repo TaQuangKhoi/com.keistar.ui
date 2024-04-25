1ebca3fa14b30df6f0328bb0b005aeac80309e9d
Tạ Quang Khôi <khoi0941@gmail.com>
2024-04-25 21:51:04 +0700

    Update findUser

--- src/bonita/api/identity/user/definitions/finds-user.ts
+++ src/bonita/api/identity/user/definitions/finds-user.ts
@@ -1,15 +1,17 @@
 /**
  * Finds Users with pagination params and filters
  */
-import {getBaseUrl} from "@/lib/axios-instance";
-import {default as axios, getBaseUrl, useBaseUrl} from "@/lib/axios-instance";
+import {default as axios, getBaseUrl} from "@/lib/axios-instance";
 
-async function findsUser(
-    p: number,
-    c: number,
-    filters: string,
+export default async function findsUser(
+    p: number = 0,
+    c: number = 10,
+    f: string,
+    o: string,
+    s: string, // search on attributes
 ) {
-    return await axios.get(getBaseUrl(`/API/identity/user/`, process.env.NEXT_PUBLIC_BONITA_HOSTNAME), {
+    const url = `/API/identity/user?p=${p}&c=${c}&f=${f}&o=${o}&s=${s}`;
+    return await axios.get(getBaseUrl(url), {
         withCredentials: true,
     }).then((response) => {
         return response.data;
