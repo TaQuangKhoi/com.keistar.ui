e2bac050e25ddf0fc8fe72ae3967133e1bcf8ff5
Tạ Quang Khôi <khoi0941@gmail.com>
2024-04-25 21:50:54 +0700

    update ot interface

--- src/app/workspace/office/ot/types/ot-inteface.ts
+++ src/app/workspace/office/ot/types/ot-inteface.ts
@@ -3,5 +3,4 @@ import KeistarItem from "@/components/keistar-ui/types/item-interface";
 export default interface OT_Item extends KeistarItem {
     ID: number,
     total_hours: string,
-    phone: string,
 }
