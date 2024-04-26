fa9755f8983ac54a3d85aeff5742b546ac89b7ac
Tạ Quang Khôi <khoi0941@gmail.com>
2024-04-26 17:18:59 +0700

    Update code

--- src/app/workspace/office/employee/employee-selected-atom.ts
+++ src/app/workspace/office/employee/employee-selected-atom.ts
@@ -1,5 +1,4 @@
-import {atom} from 'jotai'
-import { atomWithImmer } from 'jotai-immer'
+import { atomWithImmer } from 'jotai-immer';
 import Employee_Item from "@/app/workspace/office/employee/types/employee-interface";
 
 export const selectedEmployee = atomWithImmer<Employee_Item>({
---
+++ src/app/workspace/office/travel/travel-fragment.tsx
@@ -0,0 +1,7 @@
+export default function TravelFragment() {
+    return (
+        <div>
+            <h1>Travel</h1>
+        </div>
+    );
+}
