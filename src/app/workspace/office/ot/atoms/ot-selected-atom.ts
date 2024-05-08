80f16252e66f1ba4f6d4fd4ac77aaaf6f833929e
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-08 11:30:15 +0700

    Update ot-selected-atom

--- src/app/workspace/office/ot/atoms/ot-selected-atom.ts
+++ src/app/workspace/office/ot/atoms/ot-selected-atom.ts
@@ -1,5 +1,6 @@
 import {atomWithImmer} from 'jotai-immer';
 import OT_Item from "@/app/workspace/office/ot/types/ot-inteface";
+import OT_Reason from "@/app/workspace/office/ot/types/ot-reason-interface";
 
 export const selectedOtAtom = atomWithImmer<OT_Item>({
     persistenceId_string: "",
