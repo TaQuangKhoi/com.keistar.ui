152643d4b88524ac35d24f4ca15f6823ab0750df
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-08 16:09:03 +0700

    Update interface for Travel

--- src/app/workspace/office/travel/types/travel-interface.ts
+++ src/app/workspace/office/travel/types/travel-interface.ts
@@ -1,5 +1,8 @@
 import KeistarItem from "@/components/keistar-ui/types/item-interface";
+import Country_BDM from "@/app/types/country-bdm-interface";
 
 export default interface Travel_Item extends KeistarItem {
-
+    totalDays?: number;
+    country?: Country_BDM;
+    location?: string;
 }
