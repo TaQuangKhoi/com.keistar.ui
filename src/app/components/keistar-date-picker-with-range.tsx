66625d55853bb58742c11be45d346ee68cc0e7ed
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-10 16:58:50 +0700

    remove redundante code

--- src/app/components/keistar-date-picker-with-range.tsx
+++ src/app/components/keistar-date-picker-with-range.tsx
@@ -37,10 +37,6 @@ export default function KeistarDatePickerWithRange(
         setDateOnlyString?: (date: DateRangeString | undefined) => void,
     }
 ) {
-    // const [date, setDate] = React.useState<DateRange | undefined>({
-    //     from: new Date(2022, 0, 20),
-    //     to: addDays(new Date(2022, 0, 20), 20),
-    // })
 
     return (
         <div className={cn("grid gap-2", className)}>
