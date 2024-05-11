30fdf87c9371b59736e6ceee14c33ad6160d4ca7
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 17:09:04 +0700

    Done Car booking Form for Approve CB

--- src/app/workspace/office/car-booking/car-booking-form.tsx
+++ src/app/workspace/office/car-booking/car-booking-form.tsx
@@ -4,6 +4,12 @@ import {Separator} from "@/components/ui/separator";
 import CarBookingFragment from "@/app/workspace/office/car-booking/car-booking-fragment";
 import useFormItem from "@/app/workspace/hooks/use-form-item";
 import {selectedCarBookingAtom} from "@/app/workspace/office/car-booking/atoms/car-booking-selected-atom";
+import TaskSubmitFooter from "@/app/workspace/tasks/components/task-submit-footer";
+import {useAtom} from "jotai/index";
+import {tasksLoadingAtom} from "@/app/workspace/tasks/atoms/tasks-loading-atom";
+import submitReviewCarBooking from "@/app/workspace/office/car-booking/functions/submit-review-car-booking";
+import KeistarDateTimePicker from "@/app/components/keistar-date-time-picker";
+import {useState} from "react";
 
 export default function CarBookingForm({task}: { task: FullHumanTask }) {
     useFormItem({
@@ -11,11 +17,71 @@ export default function CarBookingForm({task}: { task: FullHumanTask }) {
         selectedItemAtom: selectedCarBookingAtom,
         refName: "newCarBooking_ref",
     })
+    const [, setTasksLoadingAtomValue] = useAtom(tasksLoadingAtom);
+
+
+    const [date, setDate] = useState<Date | undefined>()
+
 
     return <>
         <ProcessFormShell>
             <CarBookingFragment isInForm={true}/>
         </ProcessFormShell>
         <Separator className="mt-auto"/>
+
+        {
+            task.name === "Approve Car Booking" && (
+                <TaskSubmitFooter task={task} buttons={[
+                    {
+                        label: "Approve",
+                        onClick: async (e, comment) => {
+                            e.preventDefault()
+                            submitReviewCarBooking(task.id, comment, true)
+                            setTasksLoadingAtomValue(true);
+                        }
+                    },
+                    {
+                        label: "Reject",
+                        onClick: (e, comment) => {
+                            e.preventDefault()
+                            submitReviewCarBooking(task.id, comment, false)
+                            setTasksLoadingAtomValue(true);
+                        }
+                    },
+                ]}/>
+            )
+        }
+        {
+            task.name === "Receive Car" && (
+                <TaskSubmitFooter task={task} buttons={[
+                    {
+                        label: "Approve",
+                        onClick: async (e, comment) => {
+                            e.preventDefault()
+                            submitReviewCarBooking(task.id, comment, true)
+                            setTasksLoadingAtomValue(true);
+                        }
+                    },
+                    {
+                        label: "Reject",
+                        onClick: (e, comment) => {
+                            e.preventDefault()
+                            submitReviewCarBooking(task.id, comment, false)
+                            setTasksLoadingAtomValue(true);
+                        }
+                    },
+                ]}>
+                    <KeistarDateTimePicker
+                        date={date}
+                        setDate={setDate}
+                    />
+                </TaskSubmitFooter>
+            )
+        }
+        {
+            task.name === "Return Car" && (
+                <>Test</>
+            )
+        }
     </>
 }
