1768dc14b289b0d3ea1eb07c70c15eb1d03d358a
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 16:00:55 +0700

    Update travelProcessTaskDefinitions

--- src/app/workspace/tasks/process-definitions/travel-process.tsx
+++ src/app/workspace/tasks/process-definitions/travel-process.tsx
@@ -1,7 +1,8 @@
 import {FullHumanTask} from "@/bonita/api/bpm/human-task/types";
 import TravelForm from "@/app/workspace/office/travel/travel-form";
+import {KeistarTaskDefinition} from "@/app/workspace/tasks/process-definitions/keistar-task-definition-interface";
 
-export const travelProcessTaskDefinitions = [
+export const travelProcessTaskDefinitions: KeistarTaskDefinition[] = [
     {
         taskName: "Review Travel",
         component: (task: FullHumanTask) => <TravelForm task={task}/>,
