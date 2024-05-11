2c12275e916ca8b7b4b7d2ad7dd569b5068c7fec
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 16:02:05 +0700

    Add CarBookingInterface

---
+++ src/app/workspace/office/car-booking/types/car-booking-interface.ts
@@ -0,0 +1,18 @@
+import KeistarItem from "@/components/keistar-ui/types/item-interface";
+
+export interface AssetInterface extends KeistarItem {
+    name?: string;
+    assetLocation?: string;
+}
+
+export default interface CarBookingInterface extends KeistarItem {
+    status: string;
+    fromDate: string;
+    toDate: string;
+    purpose: string;
+    asset: AssetInterface;
+
+    // Attributes for the form only
+    fromTime?: string;
+    toTime?: string;
+}
