21387f88964982f064deacf4fc7d881ef5037f19
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 16:01:42 +0700

    add car booking form

---
+++ src/app/workspace/office/car-booking/car-booking-form.tsx
@@ -0,0 +1,7 @@
+import {FullHumanTask} from "@/bonita/api/bpm/human-task/types";
+
+export default function CarBookingForm({task}: { task: FullHumanTask }) {
+    return <>
+        Đỗ Quyên
+    </>
+}
