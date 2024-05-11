bcd34712ad73f16d832c89ab20a2d0feaccb8722
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 16:02:26 +0700

    Add config of Car Booking

---
+++ src/app/workspace/office/car-booking/config/default-car-booking.ts
@@ -0,0 +1,5 @@
+const defaultCarBooking = {
+    persistenceId: undefined,
+};
+
+export default defaultCarBooking;
---
+++ src/app/workspace/office/car-booking/config/header-car-booking.ts
@@ -0,0 +1,24 @@
+const headerCarBooking = [
+    {
+        "label": "Status",
+        "key": "status"
+    },
+    {
+        "label": "Car",
+        "key": "asset.name"
+    },
+    {
+        "label": "From Date",
+        "key": "fromDate(Date)"
+    },
+    {
+        "label": "To Date",
+        "key": "toDate(Date)"
+    },
+    {
+        "label": "Purpose",
+        "key": "purpose"
+    },
+]
+
+export default headerCarBooking;
