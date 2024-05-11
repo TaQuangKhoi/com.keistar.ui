d5a23ea55d1b4f4fa4c40519814f825703fd3dfe
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 16:15:53 +0700

    Fill data for CarBookingForm

--- src/app/workspace/office/car-booking/car-booking-form.tsx
+++ src/app/workspace/office/car-booking/car-booking-form.tsx
@@ -1,7 +1,21 @@
 import {FullHumanTask} from "@/bonita/api/bpm/human-task/types";
+import ProcessFormShell from "@/app/workspace/tasks/components/process-form-shell";
+import {Separator} from "@/components/ui/separator";
+import CarBookingFragment from "@/app/workspace/office/car-booking/car-booking-fragment";
+import useFormItem from "@/app/workspace/hooks/use-form-item";
+import {selectedCarBookingAtom} from "@/app/workspace/office/car-booking/atoms/car-booking-selected-atom";
 
 export default function CarBookingForm({task}: { task: FullHumanTask }) {
+    useFormItem({
+        taskId: task.id,
+        selectedItemAtom: selectedCarBookingAtom,
+        refName: "newCarBooking_ref",
+    })
+
     return <>
-        Đỗ Quyên
+        <ProcessFormShell>
+            <CarBookingFragment isInForm={true}/>
+        </ProcessFormShell>
+        <Separator className="mt-auto"/>
     </>
 }
