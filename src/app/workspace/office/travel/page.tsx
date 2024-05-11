7e9a50251b6369639acfc3f74e2aa4e6d4f2c763
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 09:45:06 +0700

    Use SRS in Server COmponent for Travel support metadata

--- src/app/workspace/office/travel/page.tsx
+++ src/app/workspace/office/travel/page.tsx
@@ -1,62 +1,11 @@
-'use client'
+import {Metadata} from "next";
+import TravelPage from "@/app/workspace/office/travel/travel-page";
 
-import {selectedTravelAtom} from "@/app/workspace/office/travel/atoms/travel-selected-atom";
-import {reloadTravelListAtom} from "@/app/workspace/office/travel/atoms/reload-travel-list-atom";
-import KeistarLayout from "@/components/keistar-ui/keistar-layout";
-import KeistarToolbar from "@/components/keistar-ui/keistar-toolbar";
-import KeistarLeftSidebar from "@/components/keistar-ui/keistar-left-sidebar";
-import defaultTravel from "@/app/workspace/office/travel/config/default-travel";
-import TravelFragment from "@/app/workspace/office/travel/travel-fragment";
-import headerTravel from "@/app/workspace/office/travel/config/header-travel";
-import {travelListAtom} from "@/app/workspace/office/travel/atoms/travel-list-atom";
-import {useSession} from "@/bonita/api/system/get-the-current-user-session";
-import {useEffect, useState} from "react";
+export const metadata: Metadata = {
+    title: "Travel",
+    description: "Travel module for office",
+}
 
-export default function TravelPage() {
-    const businessDataType = "com.keistar.model.office.travel.TravelRequest";
-    const [session]: [Session, boolean, any] = useSession()
-    const [username, setUsername] = useState('')
-
-    useEffect(() => {
-        console.debug(session.user_name)
-        if (session.user_name) {
-            setUsername(session.user_name)
-        }
-    }, [session]);
-
-    return KeistarLayout(
-        "Travel",
-        <KeistarToolbar reloadListAtom={reloadTravelListAtom} selectedAtom={selectedTravelAtom}
-                        defaultValue={defaultTravel}
-                        processConfig={{
-                            businessDataType,
-                            businessData: {
-                                params: {
-                                    'username': username
-                                },
-                                query: "findsOrderByUpdatedDate",
-                            },
-                            processDeletedName: "Delete_Travel",
-                            processCreateName: "Create_TravelRequest",
-                            processUpdateName: "Update_Travel",
-                        }}
-                        listAtom={travelListAtom}
-        />,
-        <KeistarLeftSidebar listAtom={travelListAtom} reloadListAtom={reloadTravelListAtom}
-                            idKey={"persistenceId_string"}
-                            titleKey={"persistenceId"}
-                            selectedAtom={selectedTravelAtom}
-                            cardConfig={{
-                                businessDataType,
-                                businessData: {
-                                    params: {
-                                        'username': username
-                                    },
-                                    query: "findsOrderByUpdatedDate",
-                                },
-                                header: headerTravel,
-                            }}
-        />,
-        <TravelFragment/>,
-    );
+export default function NextJSTravelPage() {
+    return <TravelPage/>
 }
---
+++ src/app/workspace/office/travel/travel-page.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import {selectedTravelAtom} from "@/app/workspace/office/travel/atoms/travel-selected-atom";
+import {reloadTravelListAtom} from "@/app/workspace/office/travel/atoms/reload-travel-list-atom";
+import KeistarLayout from "@/components/keistar-ui/keistar-layout";
+import KeistarToolbar from "@/components/keistar-ui/keistar-toolbar";
+import KeistarLeftSidebar from "@/components/keistar-ui/keistar-left-sidebar";
+import defaultTravel from "@/app/workspace/office/travel/config/default-travel";
+import TravelFragment from "@/app/workspace/office/travel/travel-fragment";
+import headerTravel from "@/app/workspace/office/travel/config/header-travel";
+import {travelListAtom} from "@/app/workspace/office/travel/atoms/travel-list-atom";
+import {useSession} from "@/bonita/api/system/get-the-current-user-session";
+import {useEffect, useState} from "react";
+
+
+export default function TravelPage() {
+    const businessDataType = "com.keistar.model.office.travel.TravelRequest";
+
+    const [session]: [Session, boolean, any] = useSession()
+    const [username, setUsername] = useState('')
+
+    useEffect(() => {
+        if (session.user_name) {
+            setUsername(session.user_name)
+        }
+    }, [session]);
+
+    return KeistarLayout(
+        "Travel",
+        <KeistarToolbar reloadListAtom={reloadTravelListAtom} selectedAtom={selectedTravelAtom}
+                        defaultValue={defaultTravel}
+                        processConfig={{
+                            businessDataType,
+                            businessData: {
+                                params: {
+                                    'username': username
+                                },
+                                query: "findsOrderByUpdatedDateOfUser",
+                            },
+                            processDeletedName: "Delete_Travel",
+                            processCreateName: "Create_TravelRequest",
+                            processUpdateName: "Update_Travel",
+                        }}
+                        listAtom={travelListAtom}
+        />,
+        <KeistarLeftSidebar listAtom={travelListAtom} reloadListAtom={reloadTravelListAtom}
+                            idKey={"persistenceId_string"}
+                            titleKey={"persistenceId"}
+                            selectedAtom={selectedTravelAtom}
+                            cardConfig={{
+                                businessDataType,
+                                businessData: {
+                                    params: {
+                                        'username': username
+                                    },
+                                    query: "findsOrderByUpdatedDateOfUser",
+                                },
+                                header: headerTravel,
+                            }}
+        />,
+        <TravelFragment/>,
+    );
+}
