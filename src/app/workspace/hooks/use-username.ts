60a60be07652c7f611bd7471ac4a4cc61881b0c5
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-11 16:00:06 +0700

    create custom hook useUsername

---
+++ src/app/workspace/hooks/use-username.ts
@@ -0,0 +1,15 @@
+// create for me custom hook
+import {useEffect, useState} from 'react';
+import { useSession } from '@/bonita/api/system/get-the-current-user-session';
+export function useUsername() {
+    const [session] = useSession();
+    const [username, setUsername] = useState<string>();
+
+    useEffect(() => {
+        if (session.user_name) {
+            setUsername(session.user_name);
+        }
+    }, [session]);
+
+    return [username];
+}
