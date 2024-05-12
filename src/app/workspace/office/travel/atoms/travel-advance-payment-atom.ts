906a5ee78285945f749e40560e30e6cf39c80465
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-12 14:43:31 +0700

    add travelAdvancePaymentAtom

---
+++ src/app/workspace/office/travel/atoms/travel-advance-payment-atom.ts
@@ -0,0 +1,4 @@
+import {atom} from "jotai";
+import Travel_Advance_Payment from "@/app/workspace/office/travel/types/trave-advance-payment-interface";
+
+export const travelAdvancePaymentAtom = atom<Travel_Advance_Payment[]>([])
