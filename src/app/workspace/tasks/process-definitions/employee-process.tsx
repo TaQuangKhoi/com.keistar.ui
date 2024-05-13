458cb8d98b3f40f220db4938acbe95e9115cb2bf
Tạ Quang Khôi <khoi0941@gmail.com>
2024-05-13 10:22:05 +0700

    Config task Review CV for Employee

---
+++ src/app/workspace/tasks/process-definitions/employee-process.tsx
@@ -0,0 +1,10 @@
+import {KeistarTaskDefinition} from "@/app/workspace/tasks/process-definitions/keistar-task-definition-interface";
+import {FullHumanTask} from "@/bonita/api/bpm/human-task/types";
+import EmployeeForm from "@/app/workspace/office/employee/employee-form";
+
+export const employeeProcessTaskDefinitions: KeistarTaskDefinition[] = [
+    {
+        taskName: "Review CV's",
+        component: (task: FullHumanTask) => <EmployeeForm task={task}/>,
+    }
+];
